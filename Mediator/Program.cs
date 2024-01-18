e57c0b5039bc4610142c2f49bdbdd8b6f2f907ac
jhonatan-chira <jcmar94@hotmail.com>
2024-01-17 23:28:32 -0500

    Ok

---
+++ Mediator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Mediator;
+using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediador = new Mediator();
+
+            IColleague oPepe = new User(mediador);
+            IColleague oAdmin = new UserAdmin(mediador);
+            IColleague oAdmin2 = new UserAdmin(mediador);
+
+            mediador.Add(oPepe);
+            mediador.Add(oAdmin);
+            mediador.Add(oAdmin2);
+
+            oPepe.Communicate("Oye admin tengo un problema");
+        }
+    }
+}
---
+++ Mediator/UserAdmin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public class UserAdmin : IColleague
+    {
+        public UserAdmin(IMediator mediator) : base(mediator)
+        {
+
+        }
+
+        public override void Receive(string message)
+        {
+            Console.WriteLine("Un administrador recibe: " + message);
+            Console.WriteLine("Se notifico por mail");
+        }
+    }
+}
